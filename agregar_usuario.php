1d87fbbddb85cbf084a947da529f0a04fe284437
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-07 20:26:54 +0200

    Implemented register an employee

--- agregar_usuario.php
+++ agregar_usuario.php
@@ -1,10 +1,53 @@
-<?php include "includes/header.php" ?>
+<?php
+
+include "includes/header.php";
+
+// Insertamos datos
+if (isset($_POST['crearEmpleado'])) {
+    // Obtener los valores
+    $dni = $_POST['dni'];
+    $email = $_POST['email'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $esAdmin = $_POST['es_admin'];
+
+    $query = "SELECT * FROM empleados WHERE dni = :dni";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":dni", $dni, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $registroDni = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($registroDni) {
+        $error = "Ya existe un empleado con este DNI";
+    } else {
+        // Si entra por aqui es porque e dni no existe y se puede crear el registro
+        $query = "INSERT INTO empleados(dni, email, nombre, apellido, es_admin)VALUES(:dni, :email, :nombre, :apellido, :es_admin)";
+        
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":dni", $dni, PDO::PARAM_STR);
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->bindParam(":nombre", $nombre, PDO::PARAM_STR);
+        $stmt->bindParam(":apellido", $apellido, PDO::PARAM_STR);
+        $stmt->bindParam(":es_admin", $esAdmin, PDO::PARAM_INT);
+  
+        $resultado = $stmt->execute();
+  
+        if ($resultado) {
+            $mensaje = "Empleado creado correctamente";
+        } else {
+            $error = "Error, no se pudo crear el empleado";
+        }
+    }
+  }
+
+?>
 
 
               <div class="card-header">               
                 <div class="row">
                   <div class="col-md-9">
-                    <h3 class="card-title">Lista de todos los registros usuarios</h3>
+                    <h3 class="card-title">Registrar empleado</h3>
                   </div>
                 
               </div>
@@ -14,11 +57,15 @@
               <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
                     <div class="form-group">
                         <label for="dni">DNI:</label>
-                        <input type="number" class="form-control" name="dni" placeholder="Ingresa el DNI">
+                        <input type="text" class="form-control" name="dni" placeholder="Ingresa el DNI">
                     </div>
                     <div class="form-group">
                         <label for="nombre">Nombre:</label>
                         <input type="text" class="form-control" name="nombre" placeholder="Ingresa el nombre">
+                    </div>
+                    <div class="form-group">
+                        <label for="nombre">Apellidos:</label>
+                        <input type="text" class="form-control" name="apellido" placeholder="Ingresa el/los apellidos">
                     </div>
                     <div class="form-group">
                         <label for="email">Email:</label>
@@ -32,7 +79,7 @@
                         <option value="0">No</option>                        
                         </select>
                     </div>
-                    <button type="submit" name="crearEmpleado" class="btn btn-primary w-100"><i class="fas fa-user"></i> Crear Nuevo Empleado</button>
+                    <button type="submit" name="crearEmpleado" class="btn btn-primary w-100"><i class="fas fa-user"></i> Crear nuevo empleado</button>
                     </form>
               </div>
               <!-- /.card-body -->
